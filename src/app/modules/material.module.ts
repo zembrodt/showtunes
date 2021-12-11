55c62e59c64d5d8a0b2650966d6c55d6e37d40a5
Ryan Zembrodt <ryan.zembrodt@gmail.com>
2021-12-11 11:33:15 -0500

    Update sass files to use @use instead of deprecated @import

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,33 +1,32 @@
 import { NgModule } from '@angular/core';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-import {MatDialogModule} from '@angular/material/dialog';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const materialModules = [
-  MatSliderModule,
   MatButtonModule,
-  MatDialogModule,
-  MatIconModule,
-  MatGridListModule,
   MatCardModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
   MatMenuModule,
-  MatSlideToggleModule,
   MatProgressBarModule,
   MatProgressSpinnerModule,
-  MatInputModule,
-  ReactiveFormsModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  ReactiveFormsModule
 ];
 
 @NgModule({
