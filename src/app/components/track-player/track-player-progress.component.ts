979ad63adabd128468a95ca0e03fda15e941bcc6
Ryan Zembrodt <ryan.zembrodt@gmail.com>
2022-01-16 12:44:08 -0500

    Prevent track player progress values from being negative

--- src/app/components/track-player/track-player-progress.component.ts
+++ src/app/components/track-player/track-player-progress.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input} from '@angular/core';
-import {MatSliderChange} from '@angular/material/slider';
-import {ChangeProgress} from '../../core/playback/playback.actions';
-import {Store} from '@ngxs/store';
+import { Component, Input } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
+import { Store } from '@ngxs/store';
+import { ChangeProgress } from '../../core/playback/playback.actions';
 
 @Component({
   selector: 'app-track-player-progress',
@@ -20,23 +20,22 @@ export class TrackPlayerProgressComponent {
   }
 
   getProgress(milliseconds: number): string {
-    const seconds = Math.floor((milliseconds / 1000) % 60)
-      .toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-      });
+    let seconds = Math.floor((milliseconds / 1000) % 60);
+    seconds = seconds >= 0 ? seconds : 0;
     const minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
     const hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
-    let timestamp = '';
+    let timestamp: string;
     if (hours > 0) {
-      timestamp += hours + ':' + minutes.toLocaleString('en-US', {
+      timestamp = `${hours}:${minutes.toLocaleString('en-US', {
         minimumIntegerDigits: 2,
         useGrouping: false
-      });
+      })}`;
     } else {
-      timestamp += minutes;
+      timestamp = minutes >= 0 ? minutes.toString() : '0';
     }
-    timestamp += ':' + seconds;
-    return timestamp;
+    return `${timestamp}:${seconds.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    })}`;
   }
 }
